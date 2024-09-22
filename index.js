fc72232238bc19236e50cee61dbd9722274d8a1a
hafid1994 <hafidz25.ma@gmail.com>
2024-09-22 23:49:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function hitungFaktorial(n) {
+  var hasil = 1;
+
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    for (let i = 2; i <= n; i++) {
+      hasil *= i;
+    }
+    return hasil;
+  }
+}
+
+var input = prompt("Masukkan bilangan bulat positif:");
+
+var angka = parseInt(input);
+
+// Memastikan input valid dan menghitung faktorial
+if (isNaN(angka) || angka < 0) {
+  console.log("Harap masukkan bilangan bulat positif.");
+} else {
+  var hasil = hitungFaktorial(angka);
+  alert("  Faktoroal dari " + angka + " Adalah " + hasil);
+  console.log(" Faktor dari " + angka + " adalah " + hasil);
+}
